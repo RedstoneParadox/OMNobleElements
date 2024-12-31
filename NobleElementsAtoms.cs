abecaa2c8f0252508eb2f77231e594245786637d
RedstoneParadox <spinjitzu00@gmail.com>
2024-12-31 08:17:02 -0800

    Add atom graphics

--- NobleElementsAtoms.cs
+++ NobleElementsAtoms.cs
@@ -1,7 +1,6 @@
 ﻿using AtomTypes = class_175;
 using Texture = class_256;
-using CardinalAtomColors = class_5;
-using DefaultAtomColors = class_106;
+using AtomGraphics = class_106;
 
 namespace OMNobleElements
 {
@@ -11,30 +10,39 @@ namespace OMNobleElements
 
         public static void AddAtomTypes()
         {
-            Nobilis = CreateAtomType("nobilis", 220);
+            Nobilis = CreateAtomType("nobilis", 220, prime: true);
             Alpha = CreateAtomType("alpha", 221);
             Beta = CreateAtomType("beta", 222);
             Gamma = CreateAtomType("gamma", 223);
         }
 
-        private static AtomType CreateAtomType(string name, byte id)
+        private static AtomType CreateAtomType(string name, byte id, bool prime = false)
         {
             var upperName = name[0].ToString().ToUpper() + name.Substring(1);
+            var graphics = new AtomGraphics()
+            {
+                field_994 = class_235.method_615($"noble_elements/textures/atoms/{name}_diffuse"),
+                field_995 = class_235.method_615($"noble_elements/textures/atoms/salt_shade")
+            };
+
+            if (prime)
+            {
+                graphics = new AtomGraphics()
+                {
+                    field_994 = class_235.method_615($"textures/atoms/salt_diffuse"),
+                    field_995 = class_235.method_615($"textures/atoms/salt_shade")
+                };
+            }
             
             return new AtomType()
             {
-                field_2283 = 218, /*ID*/
+                field_2283 = id, /*ID*/
                 field_2293 = false, /*Is Cardinal*/
                 field_2284 = class_134.method_254(upperName), /*Non-local Name*/
                 field_2285 = class_134.method_253($"Elemental {upperName}", string.Empty), /*Atomic Name*/
                 field_2286 = class_134.method_253(name, string.Empty), /*Local name*/
                 field_2287 = class_235.method_615($"noble_elements/textures/atoms/{name}_symbol"), /*Symbol*/
-                field_2288 = class_235.method_615($"noble_elements/textures/atoms/{name}_shadow"), /*Shadow*/
-                field_2290 = new DefaultAtomColors()
-                {
-                    field_994 = class_235.method_615($"noble_elements/textures/atoms/{name}_diffuse"),
-                    field_995 = class_235.method_615($"noble_elements/textures/atoms/{name}_shade")
-                },
+                field_2290 = graphics,
                 field_2296 = true,
                 QuintAtomType = $"NobleElements:{name}"
             };
